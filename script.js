34e11566e8d12ac8eec33fac73a7c87ad8431039
BrianCaetano <brian-caetano@hotmail.com>
2021-07-09 17:15:43 -0300

    refazendo 7 e add requisito 8

--- script.js
+++ script.js
@@ -2,16 +2,31 @@ const obj = {
   input: document.querySelector('#texto-tarefa'),
   button: document.querySelector('#criar-tarefa'),
   ol: document.querySelector('ol'),
-  li: document.querySelector('li')
+  li: document.querySelector('li'),
+  task: document.querySelectorAll('.task'),
+  selected: document.querySelector('#selected')
 }
 
+// Adiciona texto do input à uma lista pelo botão.
 obj.button.addEventListener('click', () => {
   let li = document.createElement('li');
+
   obj.ol.appendChild(li);
+  li.className = 'task';
   li.innerHTML = obj.input.value;
   obj.input.value = "";
+
+  obj.task = document.querySelectorAll('.task');
+  obj.ol = document.querySelector('ol');
 })
 
-obj.ol.addEventListener('click', event => {
-  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+// Poem fundo cinza ao clicar no item da lista.
+document.addEventListener('click', event => {
+  if (event.target.classList.contains('task')) {
+    for (let i = 0; i < obj.task.length; i += 1) {
+      obj.task[i].id = '';
+    }
+    event.target.id = 'selected';
+    obj.selected = document.querySelector('#selected');
+  }
 })
